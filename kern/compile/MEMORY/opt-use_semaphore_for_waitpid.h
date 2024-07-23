44e78656d7dfeaeeb5ace423ff057ee1b7b1dbe1
STEFANO FUMERO s323195 <s323195@studenti.polito.it>
2024-07-23 08:19:46 +0200

    base version of os161

---
+++ kern/compile/MEMORY/opt-use_semaphore_for_waitpid.h
@@ -0,0 +1,5 @@
+/* Automatically generated; do not edit */
+#ifndef _OPT_USE_SEMAPHORE_FOR_WAITPID_H_
+#define _OPT_USE_SEMAPHORE_FOR_WAITPID_H_
+#define OPT_USE_SEMAPHORE_FOR_WAITPID 1
+#endif /* _OPT_USE_SEMAPHORE_FOR_WAITPID_H_ */
---
+++ kern/compile/SHELL/opt-c2.h
@@ -0,0 +1,5 @@
+/* Automatically generated; do not edit */
+#ifndef _OPT_C2_H_
+#define _OPT_C2_H_
+#define OPT_C2 1
+#endif /* _OPT_C2_H_ */
